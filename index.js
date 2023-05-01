fd3cdaf65188394565e76c7d9be62e2f70f49a01
artenks <artenkes@gmail.com>
2023-04-30 22:38:54 -0300

    atualizando style diretorio

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const PORT = 5500;
 const homeRoute = require("./routes/home.js");
 const emojiRoute = require("./routes/emoji.js");
 
-const path = require("path");
-
 app.use(express.static(__dirname));
 
 app.use('/', homeRoute);
